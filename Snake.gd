e3d742fb7d1ea638024b841b21791ef028949b35
nitaku <matteo.abrate@gmail.com>
2019-09-17 14:24:32 +0200

    Train jam content

---
+++ Snake.gd
@@ -0,0 +1,64 @@
+extends Node2D
+
+var cell = Vector2(20, 5)
+var dir = 0
+
+var size = 10
+var tail = []
+
+var world
+
+var directions = [
+	Vector2(0, 1), # 0 right
+	Vector2(1, 1), # 1 down right
+	Vector2(1, 0), # 2 down left
+	Vector2(0, -1), # 3 left
+	Vector2(-1, -1), # 4 up left
+	Vector2(-1, 0) # 5 up right
+]
+
+func _input(event):
+	if Input.is_action_pressed("ui_right") and not(Input.is_action_pressed("ui_left")):
+		if Input.is_action_pressed("ui_up"):
+			print('Up Right')
+			dir = 5
+		elif Input.is_action_pressed("ui_down"):
+			print('Down Right')
+			dir = 1
+		else:
+			print('Right')
+			dir = 0
+	elif not(Input.is_action_pressed("ui_right")) and Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_up"):
+			print('Up Left')
+			dir = 4
+		elif Input.is_action_pressed("ui_down"):
+			print('Down Left')
+			dir = 2
+		else:
+			print('Left')
+			dir = 3
+			
+func rotate_direction():
+	dir = (dir + 4) % len(directions)
+	
+func get_direction():
+	return directions[dir]
+	
+func move(wrapping = false):
+	$Head.position = world.ij2xy(cell)
+	
+	if not wrapping:
+		tail.append(cell)
+		if len(tail) > size:
+			tail.pop_front()
+			
+		for segment in $Tail.get_children():
+			segment.free()
+			
+		for i in len(tail)-1:
+			var line = Line2D.new()
+			line.default_color = Color(1,1,1,1)
+			$Tail.add_child(line)
+			line.points = PoolVector2Array([world.ij2xy(tail[i]), world.ij2xy(tail[i+1])])
+			
---
+++ World.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+var grid = []
+const ROWS = 40
+const HEIGHT = 900
+var h = HEIGHT / ROWS
+var b = h*2.0/sqrt(3)
+
+const Point = preload('res://Point.tscn')
+
+onready var snake = $Field/Snake
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var row
+	
+	for i in ROWS:
+		row = []
+		for j in i+1:
+			var coords = ij2xy(Vector2(i, j))
+			var point = {
+				'i': i,
+				'j': j,
+				'node': Point.instance()
+			}
+			point['node'].position = Vector2(coords.x, coords.y)
+			row.append(point)
+			$Field/Grid.add_child(point['node'])
+		grid.append(row)
+		
+	# set up the snake
+	snake.world = self
+	update_snake_position()
+	
+	# movement
+	$Timer.connect('timeout', self, '_on_tick')
+	
+func _on_tick():
+	snake.cell += snake.get_direction()
+	update_snake_position()
+	
+func update_snake_position():
+	var i = int(snake.cell[0])
+	var j = int(snake.cell[1])
+	var wrapping = false
+	
+	if i >= ROWS:
+		# wrap bottom -> left
+		wrapping = true
+		snake.cell[0] = j
+		snake.cell[1] = 0
+		if snake.dir == 2:
+			snake.rotate_direction()
+	elif j >= len(grid[i]):
+		# wrap right -> bottom
+		wrapping = true
+		snake.cell[0] = ROWS-1
+		snake.cell[1] = i
+		if snake.dir == 0:
+			snake.rotate_direction()
+	elif j <= 0:
+		# wrap left -> right
+		wrapping = true
+		snake.cell[1] = len(grid[i])-1
+		if snake.dir == 4:
+			snake.rotate_direction()
+	
+	snake.move(wrapping)
+	
+func ij2xy(ij):
+	return Vector2(ij[1]*b-ij[0]*b/2.0, ij[0]*h)
+	
