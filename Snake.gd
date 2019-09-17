3edbaa5dbeb37269663db90176d3e6a97af1574f
salvob <tholalak@yahoo.it>
2019-09-17 22:29:38 +0200

    Now wrap it up

--- Snake.gd
+++ Snake.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var cell = Vector2(0, 0)
+var cell = Vector2(5, 34)
 var dir = 0
 
 var size = 10
@@ -9,26 +9,26 @@ var tail = []
 var world
 
 var directions = [
-	Vector2(0, 1), # 0 right
+	Vector2(0, 2), # 0 right
 	Vector2(1, 1), # 1 down right
-	Vector2(1, 0), # 2 down left
-	Vector2(0, -1), # 3 left
+	Vector2(1, -1), # 2 down left
+	Vector2(0, -2), # 3 left
 	Vector2(-1, -1), # 4 up left
 	Vector2(-1, 0) # 5 up right
 ]
 
 func _input(event):
-	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_right") and not dir==2:
+	if event.is_action_pressed("ui_up") and event.is_action_pressed("ui_right") and not dir==2:
 		dir = 5
-	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_left") and not dir==5:
+	elif event.is_action_pressed("ui_down") and event.is_action_pressed("ui_left") and not dir==5:
 		dir = 2
-	elif Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_left") and not dir==1:
+	elif event.is_action_pressed("ui_up") and event.is_action_pressed("ui_left") and not dir==1:
 		dir = 4
-	elif Input.is_action_pressed("ui_down") and Input.is_action_pressed("ui_right") and not dir==4:
+	elif event.is_action_pressed("ui_down") and event.is_action_pressed("ui_right") and not dir==4:
 		dir = 1
-	elif Input.is_action_pressed("ui_left") and not dir==0:
+	elif event.is_action_pressed("ui_left") and not dir==0:
 		dir = 3 
-	elif Input.is_action_pressed("ui_right") and not dir == 3:
+	elif event.is_action_pressed("ui_right") and not dir == 3:
 		dir = 0
 			
 func rotate_direction():
@@ -36,21 +36,10 @@ func rotate_direction():
 	
 func get_direction():
 	return directions[dir]
+
+func try_move(wrapping = false):
+	return not world.grid[cell.x][cell.y] == null and not "empty" in world.grid[cell.x][cell.y]
 	
 func move(wrapping = false):
-	$Head.position = world.ij2xy(cell)
+	$Head.position = world.grid[cell.x][cell.y].position
 	
-	if not wrapping:
-		tail.append(cell)
-		if len(tail) > size:
-			tail.pop_front()
-			
-		for segment in $Tail.get_children():
-			segment.free()
-			
-		for i in len(tail)-1:
-			var line = Line2D.new()
-			line.default_color = Color(1,1,1,1)
-			$Tail.add_child(line)
-			line.points = PoolVector2Array([world.ij2xy(tail[i]), world.ij2xy(tail[i+1])])
-			
--- World.gd
+++ World.gd
@@ -1,20 +1,25 @@
 extends Node2D
 
 var grid = []
-const ROWS = 10
+const ROWS = 40
 const HEIGHT = 900
 var h = HEIGHT / ROWS
 var b = h*2.0/sqrt(3)
-const OFFSET = Vector2(20, 20)
+const OFFSET = Vector2(10, 10)
+var arena = {}
+var side_a = []
+var side_b = []
+var side_c = []
 
 const Point = preload('res://Point.tscn')
 
 onready var snake = $Field/Snake
 
+
 func _ready():
 	var row
 	var this_grid = []
-	var grid = []
+	
 	for i in ROWS:
 		this_grid.append([])
 		grid.append([])
@@ -30,11 +35,13 @@ func _ready():
 			elif j>=white and j<n_points+white:
 				if r_points% 2:
 					this_grid[i].append("+")
-					grid[i].append(null)
+					grid[i].append({"empty": true})
 					r_points+=1
 					continue
 				var point = Point.instance()
 				point.position = OFFSET*Vector2(j, i)
+				point.name = str(i)+"_"+str(j)
+				point.grid_index = Vector2(i, j)
 				grid[i].append(point)
 				$Field/Grid.add_child(point)
 				this_grid[i].append("x")
@@ -42,50 +49,80 @@ func _ready():
 			else:
 				this_grid[i].append("-")
 				grid[i].append(null)
-	for i in ROWS:
-		print(this_grid[i])
-
 
 	# set up the snake
-	#snake.world = self
+	snake.world = self
 	#update_snake_position()
 	
 	# movement
-	#$Timer.connect('timeout', self, '_on_tick')
+	$Timer.connect('timeout', self, '_on_tick')
 	for i in ROWS:
-		print(grid[i])
+		for j in len(grid[i]):
+			if grid[i][j] and not i in arena:
+				arena[i] = {"start": grid[i][j], "end":grid[i][j]}
+				break
+		for j in range(ROWS*2-2, 0, -1):
+			if grid[i][j]:
+				arena[i]["end"] = grid[i][j]
+				break
+	# print(arena)
+	for a in arena:
+		# fill side_a -> right
+		side_a.append(arena[a]["end"])
+		side_b.append(arena[a]["start"])
+		for k in arena[a]:
+			print(a, ": ", k, arena[a][k].name)
+	
+	# reverse side_b for reference
+	side_b.invert()
+	for node in grid[ROWS-1]:
+		if not node == null and not "empty" in node:
+			side_c.append(node)
+	print("LATO DESTRO")
+	for node in side_a:
+		print(node.name)
+	print("LATO SINISTRO")
+	for node in side_b:
+		print(node.name)
+	print("LATO GIU")
+	for node in side_c:
+		print(node.name)
+
+func from_index_to_point(x, y):
+	return grid[x][y]
 	
 func _on_tick():
 	snake.cell += snake.get_direction()
 	update_snake_position()
 	
 func update_snake_position():
-	var i = int(snake.cell[0])
-	var j = int(snake.cell[1])
+	var point = from_index_to_point(snake.cell.x, snake.cell.y)
+	var i = int(snake.cell.x)
+	var j = int(snake.cell.y)
 	var wrapping = false
 	
-	if i >= ROWS:
-		# wrap bottom -> left
-		wrapping = true
-		snake.cell[0] = j-1
-		snake.cell[1] = 0
-		if snake.dir == 2:
-			snake.rotate_direction()
-	elif j >= len(grid[i]):
-		# wrap right -> bottom
-		wrapping = true
-		snake.cell[0] = ROWS-1
-		snake.cell[1] = i
+	if point in side_a and not snake.dir == 1 and not snake.dir == 4:
+		var index_point = side_a.find(point)
+		# rigt -> bottom
+		# we will wrap in side_c
+		snake.cell = side_c[index_point].grid_index
 		if snake.dir == 0:
 			snake.rotate_direction()
-	elif j <= 0:
-		# wrap left -> right
-		wrapping = true
-		snake.cell[1] = len(grid[i])-1
+	elif point in side_b and not snake.dir == 2 and not snake.dir == 5:
+		var index_point = side_b.find(point)
+		# we will wrap in side_a
+		snake.cell = side_a[len(side_a)-1-index_point].grid_index
 		if snake.dir == 4:
 			snake.rotate_direction()
+	elif point in side_c and not snake.dir == 0 and not snake.dir == 3:
+		var index_point = side_c.find(point)
+		# we will wrap in side_a
+		snake.cell = side_b[index_point].grid_index
+		if snake.dir == 2:
+			snake.rotate_direction()
 	
-	snake.move(wrapping)
+	if snake.try_move(wrapping):
+		snake.move(wrapping)
 	
 #func ij2xy(ij):
 #	return Vector2(ij[1]*b-ij[0]*b/2.0, ij[0]*h)
